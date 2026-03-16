052e09898a8c81a06c3e7f2175dd49a3a2094dc3
Mikaelecostax <antoniamikaele.silvacosta@gmail.com>
2026-03-16 17:57:07 +0000

    comitar

--- fup/sobrou/go/draft.go
+++ fup/sobrou/go/draft.go
@@ -2,10 +2,14 @@ package main
 import "fmt"
 func main() {
     var aq, bq, cq float64
-    var dv, ev, fv float64
     fmt.Scan(&aq, &bq, &cq)
+    var dv, ev, fv float64
     fmt.Scan(&dv, &ev, &fv)
-    total := aq*dv + bq*ev + cq*fv
-    fmt.Printf("%.2f\n", total)
+    var mk float64
+    fmt.Scan(&mk)
+    var tudo float64
+    tudo = (aq*dv) + (bq*ev) + (cq*fv)
+    troco := mk - tudo
+    fmt.Printf("%.2f\n", troco)
     
 }
