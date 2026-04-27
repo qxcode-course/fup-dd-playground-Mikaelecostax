0a1c8fba41311f9218b820dc119b791c3f961f5c
Mikaelecostax <antoniamikaele.silvacosta@gmail.com>
2026-04-27 02:26:08 +0000

    irriiiii

---
+++ fup/final/src/go/draft.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+func main() {
+    var nota1, nota2, notafinal, media, mediafinal int
+    fmt.Scan(&nota1, &nota2, &notafinal)
+    media = (nota1 + nota2) / 2
+    if media >= 7 {
+        fmt.Println("aprovado")
+    } else if media < 4 {
+        fmt.Println("reprovado")
+    } else {
+        mediafinal = (media + notafinal) / 2
+    
+    if mediafinal >=5 {
+        fmt.Println("aprovado na final")
+    } else  {
+        fmt.Println("reprovado na final")
+    }
+    }
+}
---
+++ fup/round/src/go/draft.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+func main() {
+    var r, f, c float64
+    fmt.Scan(&r, &f, &c)
+    if c {
+        fmt.Println(int(math.Ceil(numero)))
+    }
+    
+    fmt.Println(int(math.Floor(numero)))
+    fmt.Println(int(math.Round(numero)))
+    fmt.Println("Hello, World!")
+}
