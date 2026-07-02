ffd8acaa0cb4c72facc60d2feef9d939f6102224
Mikaelecostax <antoniamikaele.silvacosta@gmail.com>
2026-07-02 02:18:23 +0000

    exercicio

--- fup/jedi/src/go/draft.go
+++ fup/jedi/src/go/draft.go
@@ -1,5 +1,25 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
+    var numet int
+    fmt.Scan(&numet)
+    Jedi := 0
+    Sith := 0
+    metade := numet/2 
+    for i := 0; i < numet; i++ {
+        var forca int 
+        fmt.Scan(&forca)
+     if i < metade {
+        Jedi += forca
+    } else { 
+        Sith += forca
+    }
+    }
+    if Jedi > Sith {
+        fmt.Println("Jedi")
+    } else if Sith > Jedi {
+        fmt.Println("Sith")
+    } else {
+        fmt.Println("Empate")
+    }
 }
